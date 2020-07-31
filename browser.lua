7c3b853739d151e7595e3402901749605e0ad4f4
Eevvaann6 <48414908+Eevvaann6@users.noreply.github.com>
2020-07-31 14:35:09 -0400

    Update browser.lua

--- browser.lua
+++ browser.lua
@@ -1,3 +1,3 @@
-shell.run("wget run 'https://raw.githubusercontent.com/Eevvaann6/CC-Browser/master/.browser'")
+shell.run("wget run https://raw.githubusercontent.com/Eevvaann6/CC-Browser/master/.browser")
 term.clear()
 term.setCursorPos(1, 1)
