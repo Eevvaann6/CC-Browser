af8573437a8c1174d15ce42edb98b57d2e0da004
Eevvaann6 <48414908+Eevvaann6@users.noreply.github.com>
2020-07-31 14:55:27 -0400

    Update BrowserAutoUpdate.lua

--- BrowserAutoUpdate.lua
+++ BrowserAutoUpdate.lua
@@ -4,5 +4,5 @@ shell.run("wget https://raw.githubusercontent.com/Eevvaann6/CC-Browser/master/Br
 term.clear()
 term.setCursorPos(1, 1)
 print("Starting...")
-wait(3)
+os.sleep(3)
 shell.run(".browser")
